8edc8cbf5902ff6f0f6513722adf65d530ecdbdb
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-08 16:33:00 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,56 +1,29 @@
-class Author 
+    
+class Author
+
   attr_accessor :name, :posts
-  @@post_count = 0 
-  
+
+  @@post_count = 0
   
   def initialize(name)
     @name = name
-    posts = []
+    @posts = []
   end
-  
+
   def add_post(post)
     @posts << post
     post.author = self
-    @@post_count +=1
+    @@post_count += 1
   end
-  
+
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
     post.author = self
-    @@post_count +=1
+    @@post_count += 1
   end
 
-  
   def self.post_count
     @@post_count
-  end
-  
-end
-
-
-
-
-
-class Artist
-  attr_accessor :name
-
-  @@song_count = 0
-
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-  
-
- 
-
-  def songs
-    @songs 
-  end
-
-  def self.song_count
-    @@song_count
   end
 end
