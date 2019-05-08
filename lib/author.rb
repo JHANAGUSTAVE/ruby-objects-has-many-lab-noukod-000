9571b88b0423b21a552d2987c9f33e72e3b04aa7
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-08 16:31:34 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -21,14 +21,6 @@ class Author
     @@post_count +=1
   end
 
-  def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-  
-  
   
   def self.post_count
     @@post_count
